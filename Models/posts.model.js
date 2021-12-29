7265c31f499f2b1959ec85ec3cae0e9a0c2ba28c
Arpan Mondal <arpan19991025@gmail.com>
2021-12-29 17:40:18 +0530

    Add post model

---
+++ Models/posts.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const postSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: "Must have userId",
+      ref: "user",
+    },
+    content: {
+      type: String,
+      required: "Post content cannot be empty",
+    },
+    reactions: {
+      type: Map,
+      of: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "user",
+        },
+      ],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Post = model("post", postSchema);
+module.exports = { Post };
