9a5153d38bfa8465d0ebe3de5296a41098354cb3
Arpan Mondal <arpan19991025@gmail.com>
2022-01-13 00:10:04 +0530

    Fix: typo in folder name

--- Controllers/signup.controller.js
+++ Controllers/signup.controller.js
@@ -1,4 +1,4 @@
-const { User } = require("../models/user.model");
+const { User } = require("../Models/user.model");
 const bcrypt = require("bcrypt");
 
 const signupUser = async (req, res) => {
--- Middlewares/authentication.js
+++ Middlewares/authentication.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("../models/user.model");
+const { User } = require("../Models/user.model");
 
 const verifyAuth = async (req, res, next) => {
   try {
